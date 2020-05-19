96ab2947a38517c36588bbdcc1fe108307557b49
Yuval Saraf <unimonkiez@gmail.com>
2020-05-19 21:57:25 +0300

    Added base network with retrofit, also examples

---
+++ app/src/main/java/finalproj/dressapp/http-client/APIClient.java
@@ -0,0 +1,33 @@
+
+package com.journaldev.retrofitintro;
+
+import com.journaldev.retrofitintro.pojo.MultipleResource;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class APIClient {
+
+    private static Retrofit retrofit = null;
+
+    static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://myserverurl.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+
+
+        return retrofit;
+    }
+
+}
