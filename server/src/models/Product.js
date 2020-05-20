78103e5cd7e6e746156a31ca0b54ec61f9d9b2cd
Kobi Avshalom <kobi@Kobis-MacBook-Pro.local>
2020-05-20 10:58:24 +0300

    Added Product entity

---
+++ server/src/models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProductSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    fromdate: {
+        type: Date,
+        required: true
+    },
+    todate: {
+        type: Date,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    }
+});
+
+ProductSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) { delete ret._id }
+});
+
+module.exports = Product = mongoose.model('Product', ProductSchema, 'Products');
