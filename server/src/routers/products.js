cca5b7a2255edbad6bce700e14b897963d31c8ec
Kobi Avshalom <kobi@Kobis-MacBook-Pro.local>
2020-05-20 11:12:39 +0300

    added add product ep

---
+++ server/src/routers/products.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const validators = require('../utils/validators');
+const auth = require('../utils/auth');
+
+// Load User model
+const Product = require('../models/Product');
+
+const isProductContainErrors = (product) => {
+    if (!validators.isNonEmptyString(product.user)) return 'User cannot be empty';
+    if (!validators.isNonEmptyString(product.name)) return 'Name cannot be empty';
+    if (!validators.isPositiveNumber(product.price)) return 'Price must be positive';
+    if (!validators.isNonEmptyString(product.image)) return 'Image mcannot be empty';
+    if (!validators.isNonEmptyString(product.fromdate)) return 'From date availible cannot be empty';
+    if (!validators.isNonEmptyString(product.todate)) return 'To date availible cannot be empty';
+    return '';
+};
+
+// @route POST api/products/add
+// @desc Add product
+// @access Public
+router.post('/add', auth.isAdminLoggedIn, async (req, res) => {
+    try {
+        const { user, name, price, image, fromdate, todate } = req.body;
+        let newProduct = new Product ({
+            user,
+            name,
+            price,
+            image,
+            fromdate,
+            todate
+        });
+
+        let error = isProductContainErrors(newProduct);
+        if (error) {
+            return res.status(400).json({ error });
+        }
+
+        newProduct = await newProduct.save();
+        res.json(newProduct);
+    } catch (e) {
+        console.log(e);
+        error = 'Problem saving product';
+        res.status(400).json({ error });
+    }
+});
+
+module.exports = router;
