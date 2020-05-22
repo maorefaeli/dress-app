b1f5c7c8a4f0629b8ed1b68f20574b1e9bb99dd2
Yuval Saraf <unimonkiez@gmail.com>
2020-05-22 20:52:08 +0300

    Added CI for server and DB

--- server/src/config/keys.js
+++ server/src/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb://127.0.0.1:27018/DressApp',
+    mongoURI: 'mongodb+srv://admin:admin@cluster0-z6wuy.mongodb.net/test?retryWrites=true&w=majority', //'mongodb://127.0.0.1:27018/DressApp',
 }
