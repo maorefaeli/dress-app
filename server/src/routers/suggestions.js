4be38a3226a609a99ac7198d931c6e2c9a78fb89
Maor Refaeli <maor.refaeli@servicenow.com>
2020-08-21 20:11:06 +0300

    Add endpoint: GET /suggestions

--- server/src/routers/suggestions.js
+++ server/src/routers/suggestions.js
@@ -2,15 +2,45 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../utils/auth');
 const WishlistController = require('../controllers/wishlistController');
-const User = require('../models/User');
+const PendingCycle = require('../models/PendingCycle');
 const ObjectID = require('mongodb').ObjectID;
 
 const MAXIMUM_MILLISECONDS_FOR_SUGGESTION_REQUEST = 7 /* days */ * 24 * 60 * 60 * 1000;
 
+// @route GET /suggestions
+// @desc Get smart suggestions for the user. Output: [{cycleId, product}]
+// @access Private
+router.get('/', auth.isLoggedIn, async (req, res) => {
+    try {
+        const result = [];
+        const userId = ObjectID(req.user.id);
+        const cycles = await PendingCycle.find({ 'participants.user': userId }).populate('participants.products') || [];
+
+        for (const cycle of cycles) {
+            for (const participant of cycle.participants) {
+                if (participant.user.equals(userId)) {
+                    participant.products.forEach(product => {
+                        result.push({
+                            cycleId: cycle._id,
+                            product
+                        });
+                    });
+                    break;
+                }
+            }
+        }
+
+        return res.json(result);
+    } catch (error){
+        console.log(error);
+        res.status(400).json({"error":"Problem getting suggestions"});
+    }
+});
+
 // @route POST /suggestions/request
 // @desc Request a suggestion
 // @access Private
-router.post('/suggestions/request', auth.isLoggedIn, async (req, res) => {
+router.post('/request', auth.isLoggedIn, async (req, res) => {
     try {
         const { cycle, product, fromdate, todate } = req.body;
 
