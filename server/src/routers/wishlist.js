c8fb11c9890afdd5f6766d47ab8546faea36a5bd
Maor Refaeli <maor.refaeli@servicenow.com>
2020-08-21 18:28:39 +0300

    Add endpoint: GET /wishlist

--- server/src/routers/wishlist.js
+++ server/src/routers/wishlist.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../utils/auth');
 const WishlistController = require('../controllers/wishlistController');
+const User = require('../models/User');
 
 // @route POST /wishlist/add
 // @desc Add new product to wishlist
@@ -14,6 +15,27 @@ router.post('/add', auth.isLoggedIn, async (req, res) => {
     } catch (error){
         console.log(error);
         res.status(400).json({"error":"Problem adding product to wishlist"});
+    }
+});
+
+// @route GET /wishlist
+// @desc Get the logged in user wishlist
+// @access Private
+router.get('/', auth.isLoggedIn, async (req, res) => {
+    try {
+        let result = [];
+
+        const user = await User.findById(req.user.id).populate('wishlist.products');
+        if (user.wishlist) {
+            for (const wish of user.wishlist) {
+                result = [...result, ...wish.products];
+            }
+        }
+
+        return res.json(result);
+    } catch (error){
+        console.log(error);
+        res.status(400).json({"error":"Problem finding user wishlist"});
     }
 });
 
