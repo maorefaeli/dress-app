fd60fadf6968d199c4bee06620a2d9c2ddad276e
Soraky <sdoraky16@gmail.com>
2020-08-23 17:25:43 +0300

    Updated rent model

--- app/src/main/java/finalproj/dressapp/httpclient/models/RentProduct.java
+++ app/src/main/java/finalproj/dressapp/httpclient/models/RentProduct.java
@@ -3,12 +3,17 @@ package finalproj.dressapp.httpclient.models;
 import android.widget.ImageView;
 
 import com.google.gson.annotations.SerializedName;
+import finalproj.dressapp.httpclient.models.UserRegistration;
+import finalproj.dressapp.httpclient.models.Product;
 
 import java.util.Date;
 
 public class RentProduct {
     @SerializedName("product")
-    public String product;
+    public Product product;
+
+    @SerializedName("user")
+    public UserRegistration user;
 
     @SerializedName("fromdate")
     public String fromdate;
@@ -16,8 +21,11 @@ public class RentProduct {
     @SerializedName("todate")
     public String todate;
 
+    @SerializedName("coins")
+    public int coins;
+
     public RentProduct(String product, String fromdate, String todate) {
-        this.product = product;
+        this.product.id = product;
         this.fromdate = fromdate;
         this.todate = todate;
     }
