17d1386ad5f5f2c23a027b50c02cffdb3f5de7c4
Maor Refaeli <maor.refaeli@servicenow.com>
2020-08-23 09:50:36 +0300

    Add rentingDates to Product

--- app/src/main/java/finalproj/dressapp/httpclient/models/Product.java
+++ app/src/main/java/finalproj/dressapp/httpclient/models/Product.java
@@ -1,10 +1,16 @@
 package finalproj.dressapp.httpclient.models;
 
-import android.widget.ImageView;
-
 import com.google.gson.annotations.SerializedName;
 
-import java.util.Date;
+import java.util.List;
+
+class RentingDate {
+    @SerializedName("fromdate")
+    public String fromDate;
+
+    @SerializedName("todate")
+    public String toDate;
+}
 
 public class Product {
     @SerializedName("user")
@@ -28,14 +34,15 @@ public class Product {
     @SerializedName("image")
     public String image;
 
-    @SerializedName("isInWishlist")
-    public Boolean isInWishlist;
+    @SerializedName("rentingDates")
+    public List<RentingDate> rentingDates;
 
-    public Product(String productName, Number price, String fromDate, String toDate, String image) {
+    public Product(String productName, Number price, String fromDate, String toDate, String image, List<RentingDate> rentingDates) {
         this.name = productName;
         this.price = price;
         this.fromdate = fromDate;
         this.todate = toDate;    
-        this.image = image;    
+        this.image = image;
+        this.rentingDates = rentingDates;
     }
 }
